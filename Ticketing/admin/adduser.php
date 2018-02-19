4a75ea05b97db079f1f889120b55fb9bc06c766c
bryanpau1 <36620507+bryanpau1@users.noreply.github.com>
2018-02-19 17:10:30 +0800

    Add files via upload
    change adduser as it was still being developed change it back.

--- Ticketing/admin/adduser.php
+++ Ticketing/admin/adduser.php
@@ -1,53 +1,13 @@
-<?php 
+<?php
 	include('conn.php');
-	
-	if (isset($_POST['username_check'])) {
-		$username = $_POST['username'];
-
-		$sql = "SELECT * FROM users WHERE username='$username'";
-		$results = mysqli_query($db, $sql);
-
-		if (mysqli_num_rows($results) > 0) {
-			echo "taken";	
-		}else{
-			echo 'not_taken';
-		}
-		exit();
-	}
-
-	if (isset($_POST['email_check'])) {
-		$email = $_POST['email'];
-
-		$sql = "SELECT * FROM users WHERE email='$email'";
-		$results = mysqli_query($db, $sql);
-
-		if (mysqli_num_rows($results) > 0) {
-			echo "taken";	
-		}else{
-			echo 'not_taken';
-		}
-		exit();
-	}
-
-	if (isset($_POST['susername'])) {
+	if(isset($_POST['susername'])){
+		$product_name=$_POST['susername'];
 		$name=$_POST['name'];
 		$email=$_POST['email'];
 		$username=$_POST['susername'];
 		$password=$_POST['spassword'];
 
-		$sql = "SELECT * FROM users WHERE username='$username'";
-		$results = mysqli_query($db, $sql);
-
-		if (mysqli_num_rows($results) > 0) {
-			echo "exists";	
-			exit();
-		}else{
-			$query = "INSERT INTO users (name, email,username, password, level) 
-					VALUES ('$name',' '$username', '$email', ',".md5($password)."','agent')";
-			$results = mysqli_query($db, $query);
-			echo 'Saved!';
-			exit();
-		}
+		$conn->query("insert into users (name, email,username, password, level) values ('$name','$email','$username', '$mpassword','agent')");
 	}
 
 ?>
